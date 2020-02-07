817d167b6ff11b6af70dd7ef91ad266279b814a7
yzngo <yzngo@live.com>
2020-02-07 14:42:38 +0800

    update

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = .2f;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate() {
+        Vector3 targetPosition = new Vector3(
+            target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(
+            transform.position, targetPosition, ref _velocity, smoothTime);
+    }
+}
